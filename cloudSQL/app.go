7bd84ff725a3dd613eea7c95e62c1a9706593ec5
Sam Chu <samuelechu@gmail.com>
2017-08-07 11:29:31 -0700

    fd

--- cloudSQL/app.go
+++ cloudSQL/app.go
@@ -80,7 +80,7 @@ func testConn(dbOpenString string) error {
                 return err
         }
 
-        _, err := db.Query("SHOW DATABASES")
+        _, err = db.Query("SHOW DATABASES")
 
         if err != nil{
                 return err
