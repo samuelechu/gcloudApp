63ddb0112f3e907d1f3d5529d2a61165a551a479
Samuel Chu <samuelechu@gmail.com>
2017-08-18 14:33:36 -0700

    post request

--- hello/hello.go
+++ hello/hello.go
@@ -44,9 +44,9 @@ func checkToken(w http.ResponseWriter, r *http.Request) {
     
     urlStr := "/oauth2/v4/token"
 
-    redirectUri := "https://gotesting-175718.appspot.com"
+    redirectUri := "https%3a%2f%2fgotesting-175718.appspot.com"
     if appengine.IsDevAppServer(){
-        redirectUri = "https://8080-dot-2979131-dot-devshell.appspot.com"
+        redirectUri = "https%3a%2f%2f8080-dot-2979131-dot-devshell.appspot.com"
     }
 
     bodyVals := url.Values{
