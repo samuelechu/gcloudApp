cb25f0ff8ffe2233c355d2d9b860d0fa79da74b7
Samuel Chu <samuelechu@gmail.com>
2017-08-20 15:52:38 -0700

    fdf

--- cloudSQL/cloudDatabase.go
+++ cloudSQL/cloudDatabase.go
@@ -16,7 +16,7 @@ import (
         _ "github.com/go-sql-driver/mysql"
 )
 
-var db sql.DB
+var db *sql.DB
 
 func init() {
         initDB()
@@ -36,7 +36,7 @@ func initDB(){
             dbOpenString = fmt.Sprintf("%s:%s@tcp([localhost]:3306)/%s", user, password, dbName)
     }
 
-    db, err = sql.Open("mysql", dbOpenString)
+    db, err := sql.Open("mysql", dbOpenString)
 
     if err != nil {
         log.Print("Could not open db: %v", err)
