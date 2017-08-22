9c3900d4bd2d05c0bc38f4b98bc8123b82f3d124
Samuel Chu <samuelechu@gmail.com>
2017-08-21 20:35:17 -0700

    FdF

--- hello/static/scripts/signIn.js
+++ hello/static/scripts/signIn.js
@@ -1,3 +1,4 @@
+
 function setElements(isLoggedIn){
   if(isLoggedIn){
       document.getElementById('gSignInButton').style.display = 'none'
@@ -15,4 +16,25 @@ function signOut() {
     console.log('User signed out.');
   });
   setElements(false);
+}
+
+ function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + id_token);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+      console.log('Signed in as: ' + xhr.responseText);
+    };
+    xhr.send('idtoken=' + id_token);
+    
+
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('id_token: ' + id_token);
+    setElements(true);
 }
