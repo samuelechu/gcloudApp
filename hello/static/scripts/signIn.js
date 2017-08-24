086eb3e1009f1634855de5066985da07ce7d801f
Samuel Chu <samuelechu@gmail.com>
2017-08-24 16:03:14 -0700

    Fdf

--- hello/static/scripts/signIn.js
+++ hello/static/scripts/signIn.js
@@ -13,6 +13,7 @@ function setElements(isLoggedIn){
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
+    $('.collapse').collapse("hide")
     console.log('User signed out.');
   });
   setElements(false);
@@ -25,6 +26,9 @@ function sendTokentoDB(googleUser, id_token){
   var xhr = new XMLHttpRequest();
   xhr.open('POST', 'signIn');
   xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onload = function() {
+    $('.collapse').collapse("show")
+  };
 
   var data = {
       Uid : id_token
