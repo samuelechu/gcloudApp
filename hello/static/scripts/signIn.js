1ba7deb34f1498083805b8de3726861a5c37ed0e
Samuel Chu <samuelechu@gmail.com>
2017-08-26 10:08:37 -0700

    Fdf

--- hello/static/scripts/signIn.js
+++ hello/static/scripts/signIn.js
@@ -13,6 +13,7 @@ function setElements(isLoggedIn){
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
+    $("#selectSection").addClass('hidden');
     console.log('User signed out.');
   });
   setElements(false);
