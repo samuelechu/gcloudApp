90ed0354280374b9fec0137ca42efc52e7435ac3
Samuel Chu <samuelechu@gmail.com>
2017-08-29 18:19:13 -0700

    Fdf

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -82,7 +82,7 @@ func askPermissions(w http.ResponseWriter, r *http.Request) {
 	}
 
     queryVals := url.Values{
-        "scope" : {"https://www.googleapis.com/auth/gmail.readonly profile"},
+        "scope" : {"profile"},
         "access_type" : {"offline"},
         "include_granted_scopes" : {"true"},
         "prompt" : {"consent"},
