aa327f4b650130d0f7a07d3aff59a8d0b90c010d
Samuel Chu <samuelechu@gmail.com>
2017-08-30 15:54:19 -0700

    Fdf

---
+++ oauth/oauthHelpers.go
@@ -0,0 +1,55 @@
+package oauth
+
+import (
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
+	"fmt"
+	"log"
+	"bytes"
+	"os"
+	"io/ioutil"
+    "encoding/json"
+	"net/http"
+	"net/url"
+)
+
+
+func getJSONRespBody(r *http.Request, url string, data url.Values, rb interface{}) interface{} {
+
+	body := bytes.NewBufferString(data.Encode())
+
+    log.Print(body)
+    req, _ := http.NewRequest("POST", url, body)
+    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+    log.Print("finished marshaling")
+
+    ctx := appengine.NewContext(r)
+    client := urlfetch.Client(ctx)
+
+    resp, err := client.Do(req)
+    if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+    }
+    log.Print("am here")
+    defer resp.Body.Close()
+    respBody, _ := ioutil.ReadAll(resp.Body)
+    log.Printf("HTTP Post returned %v", string(respBody))
+    // fmt.Fprintf(w, "HTTP Post returned %v", string(respBody))
+
+    if resp.Body == nil {
+        http.Error(w, "Response body not found", 400)
+        return
+    }
+
+    err = json.Unmarshal(respBody, &rb)
+    if err != nil {
+        http.Error(w, err.Error(), 400)
+        return
+    }
+
+    return rb
+    
+    
+}
