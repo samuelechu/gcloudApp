0ae22f6e75845f080a0f91cce708cae5594e0bd7
Samuel Chu <samuelechu@gmail.com>
2017-08-31 16:04:59 -0700

    wow

--- cloudSQL/cloudDatabase.go
+++ cloudSQL/cloudDatabase.go
@@ -98,16 +98,17 @@ func signInHandler(w http.ResponseWriter, r *http.Request) {
                 return
     }
 
-    var u jsonHelper.User
+    var u, user jsonHelper.User
     if u, ok := jsonHelper.UnmarshalJSON(w, r, r.Body, u).(jsonHelper.User); ok {
-        log.Printf("UnmarshalJSON returned %v %v", u.Uid, u.Name)
+        user = u
+        log.Printf("UnmarshalJSON returned %v %v", user.Uid, user.Name)
 
     }
 
     stmt, err := db.Prepare("INSERT IGNORE INTO users SET uid=?, Name=?")
     checkErr(err)
 
-    res, err := stmt.Exec(u.Uid, u.Name)
+    res, err := stmt.Exec(user.Uid, user.Name)
     checkErr(err)
 
     id, err := res.RowsAffected()
