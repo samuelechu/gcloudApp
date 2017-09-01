fb2806dacfc26fb55d87c5a2c9d88d8fcf33a4b0
Samuel Chu <samuelechu@umail.ucsb.edu>
2017-09-01 14:19:53 -0700

    fdf

--- templateTest/templateTest.go
+++ templateTest/templateTest.go
@@ -9,11 +9,15 @@ func init() {
         http.HandleFunc("/testTemplate", handler)
 }
 
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    t, _ := template.ParseFiles("header.html", "footer.html")
-    t.Execute(w, map[string] string {"Title": "My title", "Body": "Hi this is my body"})
+type Person struct {
+    Title string
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+    t, _ := template.ParseFiles("header.html")
+    p := Person{Title: "Astaxie"}
+    t.Execute(w, p)
+
+}
 
 
