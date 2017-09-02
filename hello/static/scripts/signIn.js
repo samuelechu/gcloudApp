37afcbf3278ecbbbadd4a5db70513262864fbce3
Samuel Chu <samuelechu@gmail.com>
2017-09-02 16:03:32 -0700

    Fdf

--- hello/static/scripts/signIn.js
+++ hello/static/scripts/signIn.js
@@ -70,7 +70,7 @@ function onSignIn(googleUser) {
 
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
+  console.log('Name: ' +  '{{.UserName}}');//profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   console.log('id_token: ' + id_token);
