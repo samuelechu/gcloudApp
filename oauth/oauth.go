6b2c79d9bf96076ceed33265ef6fca1ed07dabfd
Samuel Chu <samuelechu@gmail.com>
2017-09-03 22:04:01 -0700

    Fdf

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -118,13 +118,13 @@ func oauthCallback(w http.ResponseWriter, r *http.Request) {
     //store the user and refresh token into database
     cloudSQL.InsertUser(uid, name, respBody.Refresh_token)
     
-    jsonHelper.MarshalJSON(w, r, jsonHelper.User{Uid : uid, Name: name})
-    
     redirectString := "https://gotesting-175718.appspot.com"
     if appengine.IsDevAppServer(){
         redirectString = "https://8080-dot-2979131-dot-devshell.appspot.com"
     }
 
+    log.Printf("The type is %v", r.URL.Query().Get("type"))
+    
     http.SetCookie(w, &http.Cookie{
         Name: r.URL.Query().Get("type"),
         Value: respBody.Id_token,
