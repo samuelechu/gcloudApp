fd3ff86df71c7f5a738e4d0a011ccf7173e7f509
Samuel Chu <samuelechu@gmail.com>
2017-09-04 22:09:30 -0700

    cookies

--- oauth/oauthHelpers.go
+++ oauth/oauthHelpers.go
@@ -43,7 +43,7 @@ func GetCookies(w http.ResponseWriter, r *http.Request) {
     deleteCookies(w, r)
 
     if curCookies == nil {
-        curCookies = cookies{}
+        curCookies = &cookies{}
         return
     }
 
