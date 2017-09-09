da34f6fa12a5504265dd82cb0ca5f886dcc08f49
Samuel Chu <samuelechu@gmail.com>
2017-09-09 09:12:23 -0700

    Fdf

--- transferMail/transferMail.go
+++ transferMail/transferMail.go
@@ -62,7 +62,8 @@ func transferEmail(w http.ResponseWriter, r *http.Request) {
     // }
     
     jsonparser.ArrayEach(respBody, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-        log.Println(jsonparser.Get(value, "id"))
+        id, _, _, _ := jsonparser.Get(value, "id")
+        log.Println(id)
     }, "threads")
 
 
