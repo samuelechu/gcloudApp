d682714c16065d77cababafb7d87a698af9e22fb
Samuel Chu <samuelechu@gmail.com>
2017-09-09 23:40:28 -0700

    Fdf

--- transferMail/transferMailHelper.go
+++ transferMail/transferMailHelper.go
@@ -2,13 +2,7 @@ package transferMail
 
 import (
 	"log"
-	"net/http"
-    "io/ioutil"
-    "google.golang.org/appengine"
-    "google.golang.org/appengine/urlfetch"
-	"github.com/samuelechu/oauth"
     "github.com/samuelechu/cloudSQL"
-    "github.com/buger/jsonparser"
 )
 
 func startTransfer(curUserID, sourceToken, sourceID, destToken, destID string) {
