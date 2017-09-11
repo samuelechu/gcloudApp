7504835a3f0e365656025c7db67fb71b3e26a303
Samuel Chu <samuelechu@gmail.com>
2017-09-11 11:09:19 -0700

    Fdf

--- transferMail/transferMailHelper.go
+++ transferMail/transferMailHelper.go
@@ -9,6 +9,9 @@ import (
     "time"
     "github.com/samuelechu/cloudSQL"
     "github.com/buger/jsonparser"
+    "google.golang.org/appengine"
+    "google.golang.org/appengine/urlfetch"
+    "google.golang.org/appengine/runtime"
 )
 
 type nopCloser struct { 
