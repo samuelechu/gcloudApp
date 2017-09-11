bb900929bb1902d20b8678db1e715ca2b4b4fdd3
Samuel Chu <samuelechu@gmail.com>
2017-09-10 20:47:26 -0700

    Fdf

--- transferMail/transferMailHelper.go
+++ transferMail/transferMailHelper.go
@@ -4,8 +4,7 @@ import (
     "log"
 	"net/http"
     "io/ioutil"
-    "google.golang.org/appengine"
-    "google.golang.org/appengine/urlfetch"
+    "time"
     "github.com/samuelechu/cloudSQL"
     //"github.com/buger/jsonparser"
 )
