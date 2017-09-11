5f8faa8ae0243be43da0de97ecfafc65ea5310f9
Samuel Chu <samuelechu@gmail.com>
2017-09-10 22:08:57 -0700

    Fdf

--- transferMail/transferMailHelper.go
+++ transferMail/transferMailHelper.go
@@ -65,9 +65,9 @@ func startTransfer(curUserID, sourceToken, sourceID, destToken, destID string) {
 
     urlStr = "https://www.googleapis.com/upload/gmail/v1/users/me/messages?uploadType=multipart"
 
-    body := strings.NewReader('--foobar\nContent-Type: application/json; charset=UTF-8\n{' +
-'\n\"raw\":\"' + raw + '\"\n\"labelIds\": ["INBOX", "UNREAD"]\n}' +
-'--foo_bar\nContent-Type: message/rfc822\n\nstringd\n--foo_bar--')
+    body := strings.NewReader("--foobar\nContent-Type: application/json; charset=UTF-8\n{" +
+"\n\"raw\":\"" + raw + "\"\n\"labelIds\": ["INBOX", "UNREAD"]\n}" +
+"--foo_bar\nContent-Type: message/rfc822\n\nstringd\n--foo_bar--")
 
     insertReq, _ := http.NewRequest("POST", urlStr, body)
     insertReq.Header.Set("Authorization", "Bearer " + destToken)
