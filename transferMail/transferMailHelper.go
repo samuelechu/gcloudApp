fd75fd99a0d0a66bfb1f73786fdb4a5fb40a9b84
Samuel Chu <samuelechu@gmail.com>
2017-09-10 23:16:21 -0700

    Fdf

--- transferMail/transferMailHelper.go
+++ transferMail/transferMailHelper.go
@@ -107,8 +107,8 @@ func startTransfer(curUserID, sourceToken, sourceID, destToken, destID string) {
     respBody, _ = ioutil.ReadAll(body)
     log.Printf("HTTP PostForm/GET returned %v", string(respBody))
 
-//     log.Print("--foo_bar\nContent-Type: application/json; charset=UTF-8\n{" +
-// "\n\"raw\":\"" + rawReal + "\",\n\"labelIds\": [\"INBOX\", \"UNREAD\"]\n}" +
-// "--foo_bar\nContent-Type: message/rfc822\n\nstringd\n--foo_bar--")
+    log.Print("--foo_bar\nContent-Type: application/json; charset=UTF-8\n{" +
+"\n\"raw\":\"" + rawReal + "\",\n\"labelIds\": [\"INBOX\", \"UNREAD\"]\n}" +
+"--foo_bar\nContent-Type: message/rfc822\n\nstringd\n--foo_bar--")
 
 }
