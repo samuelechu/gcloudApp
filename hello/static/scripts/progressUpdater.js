d4242921eff94c91d9011209a18968878b0fb106
Samuel Chu <samuelechu@gmail.com>
2017-09-18 16:24:40 -0700

    Fdf

--- hello/static/scripts/progressUpdater.js
+++ hello/static/scripts/progressUpdater.js
@@ -1,9 +1,14 @@
 
-function updateProgress(){
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'jobInfo?uid=' + uid);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function() {
+function updateProgress(uid){
+	var auth2 = gapi.auth2.getAuthInstance();
+	var googleUser= auth2.currentUser.get();
+	var profile = googleUser.getBasicProfile()
+	var uid = profile.getId()
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'jobInfo?uid=' + uid);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  	xhr.onload = function() {
     console.log('Job in progress : ' + xhr.responseText);
     var resp = JSON.parse(xhr.responseText);
     var percentage = resp.Processed_threads * 100 / resp.Total_threads
